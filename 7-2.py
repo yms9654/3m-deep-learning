2ed0095315325a0445d3479edc2121cb9f9d5207
minsik <yms9654@gmail.com>
2017-10-27 12:58:35 +0900

    update

--- 7-2.py
+++ 7-2.py
@@ -5,26 +5,21 @@ mnist = input_data.read_data_sets("./mnist/data/", one_hot=True)
 
 X = tf.placeholder(tf.float32, [None, 28, 28, 1])
 Y = tf.placeholder(tf.float32, [None, 10])
-keep_prob = tf.placeholder(tf.float32)
+is_training = tf.placeholder(tf.bool)
 
-W1 = tf.Variable(tf.random_normal([3, 3, 1, 32], stddev=0.01))
-L1 = tf.nn.conv2d(X, W1, strides=[1, 1, 1, 1], padding="SAME")
-L1 = tf.nn.relu(L1)
-L1 = tf.nn.max_pool(L1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+L1 = tf.layers.conv2d(X, 32, [3, 3])
+L1 = tf.layers.max_pooling2d(L1, [2, 2], [2, 2])
+L1 = tf.layers.dropout(L1, 0.7, is_training)
 
-W2 = tf.Variable(tf.random_normal([3, 3, 32, 64], stddev=0.01))
-L2 = tf.nn.conv2d(L1, W2, strides=[1, 1, 1, 1], padding='SAME')
-L2 = tf.nn.relu(L2)
-L2 = tf.nn.max_pool(L2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+L2 = tf.layers.conv2d(L1, 64, [3, 3])
+L2 = tf.layers.max_pooling2d(L2, [2, 2], [2, 2])
+L2 = tf.layers.dropout(L2, 0.7, is_training)
 
-W3 = tf.Variable(tf.random_normal([7 * 7 * 64, 256], stddev=0.01))
-L3 = tf.reshape(L2, [-1, 7 * 7 * 64])
-L3 = tf.matmul(L3, W3)
-L3 = tf.nn.relu(L3)
-L3 = tf.nn.dropout(L3, keep_prob)
+L3 = tf.contrib.layers.flatten(L2)
+L3 = tf.layers.dense(L3, 256, activation=tf.nn.relu)
+L3 = tf.layers.dropout(L3, 0.5, is_training)
 
-W4 = tf.Variable(tf.random_normal([256, 10], stddev=0.01))
-model = tf.matmul(L3, W4)
+model = tf.layers.dense(L3, 10, activation=None)
 
 cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
     logits=model, labels=Y
@@ -36,7 +31,7 @@ init = tf.global_variables_initializer()
 sess = tf.Session()
 sess.run(init)
 
-batch_size = 50
+batch_size = 100
 total_batch = int(mnist.train.num_examples / batch_size)
 
 for epoch in range(15):
@@ -49,7 +44,7 @@ for epoch in range(15):
        _, cost_val = sess.run([optimizer, cost],
                               feed_dict={X: batch_xs,
                                          Y: batch_ys,
-                                         keep_prob: 0.7})
+                                         is_training: True})
        total_cost += cost_val
 
     print('Epoch:', '%04d' % (epoch + 1),
@@ -62,4 +57,4 @@ accuracy = tf.reduce_mean(tf.cast(is_correct, tf.float32))
 print('정확도:', sess.run(accuracy,
                        feed_dict={X: mnist.test.images.reshape(-1, 28, 28, 1),
                                   Y: mnist.test.labels,
-                                  keep_prob: 1}))
+                                  is_training: False}))
